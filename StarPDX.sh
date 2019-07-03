8d4b58312c5004cd70c4a267d973ffe1d7dad6d0
bgudenas <bgudena@g.clemson.edu>
2019-07-03 15:32:43 -0500

    Add htseqcount and fix bam sort

--- StarPDX.sh
+++ StarPDX.sh
@@ -1,58 +1,5 @@
 #!/bin/bash
 
-echo "#########"
-echo "Get Reads"
-echo 'FDIR = ' $1
-FDIR=$1
-echo 'NCUT = ' $2
-NCUT=$2
-echo 'HCUT = ' $3
-HCUT=$3
-echo 'LSB_JOBINDEX = ' $4
-LSB_JOBINDEX=$4
-echo 'RUN = ' $5
-RUN=$5
-
-FASTQS=$( find ${FDIR}* -type f -name "*q.gz" -exec ls {} + )
-SAMP=$( ls $FASTQS | rev | cut -c ${NCUT}- | rev  |sed 's!.*/!!' | sed 's/_$//' | sort | uniq -d | awk -v var="$LSB_JOBINDEX" 'FNR == var {print}' )
-
-[[ !  -z  ${SAMP// /}  ]] || { echo >&2 "SAMPLE ID is EMPTY"; exit 1; }
-
-## Cut sample run ID header if needed
-if (( HCUT > 0 ));
-then
-SAMP=$( echo $SAMP | cut -c ${HCUT}- )
-fi
-
-printf "\n"
-echo "SAMPLE####################"
-echo $SAMP
-R1=$( ls $FASTQS| grep ${SAMP} | grep "R1" )
-R2=$(  ls $FASTQS  | grep ${SAMP} | grep "R2" )
-
-## Abort if not same number of read1 and read2 fastqs
-if (( $( ls $R1 | wc -l ) != $( ls $R2 | wc -l ) )); 
-then
-echo >&2 "FASTQS DONT MATCH"
-exit 1
-fi
-
-echo "READ1####"
-ls -lh $R1
-echo "READ2####"
-ls -lh $R2
-
-MR1="${TMPDIR}/${SAMP}_R1.fastq"
-MR2="${TMPDIR}/${SAMP}_R2.fastq"
-
-if [ "$RUN" = "YES" ]
-	then
-cat $R1 | gunzip > $MR1
-cat $R2 | gunzip > $MR2
-fi
-[bgudenas@hpc01 PDX]$ cat ~/src/StarPDX.sh 
-#!/bin/bash
-
 echo "starPDX"
 THR=$1
 
@@ -94,7 +41,7 @@ if [ ! -f  $SORTBAM ]; ## if Bam does not exist
     --outFilterMultimapNmax 20 \
     --alignSJDBoverhangMin 1
 
-    sambamba sort -t $THR -m 45G --tmpdir $TMPDIR \
+    sambamba sort -t $THR -m 38G --tmpdir $TMPDIR \
 	--sort-by-name \
 	-o $SORTBAM $INBAM 
 #  rm $INBAM
@@ -119,18 +66,25 @@ if [ ! -f  $SORTBAM ]; ## if Bam does not exist
     --outFilterMultimapNmax 20 \
     --alignSJDBoverhangMin 1
 
-    sambamba sort -t $THR -m 45G --tmpdir $TMPDIR \
-	--sort-by-name \  
-	-o $SORTBAM $INBAM 
+    sambamba sort -t $THR -m 38G --tmpdir $TMPDIR \
+    --sort-by-name \
+    -o $SORTBAM $INBAM
  # rm $INBAM
 fi
 
-
+if [ ! -f ./Disambig/disambiguatedSpeciesA.bam ]
+	then
 module load disambiguate
 disambiguate.py -s ${SAMP} \
 		./Data/Human/${SAMP}_Sorted.bam \
-		./Data/Human/${SAMP}_Sorted.bam  \
+		./Data/Mouse/${SAMP}_Sorted.bam  \
 		-i $TMPDIR \
 		-a star \
 		-d \
 		-o Disambig
+fi
+
+module load htseq/0.6.1p1
+
+htseq-count -f bam -r order name -s yes \
+./Disambig/disambiguatedSpeciesA.bam /home/bgudenas/Annots/Human/Homo_sapiens.GRCh38.93.gtf > Counts/${SAMP}_Counts.txt
