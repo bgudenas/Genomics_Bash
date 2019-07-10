4bc609762d5007111f029c343171921d0ba05603
bgudenas <bgudena@g.clemson.edu>
2019-07-10 14:56:14 -0500

    Works with adap trim or getReads now

--- StarPDX.sh
+++ StarPDX.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
+echo "#########"
+echo "Adapter Trim"
+echo 'Threads = ' $1
+THR=$1
+
+module purge
+module load bbmap/37.28
+ADAPTERS=/home/bgudenas/Annots/adapters.fa
+
+
+R1=$(ls $TMPDIR/*R1.fastq )
+ls $R1
+R2=$(ls $TMPDIR/*R2.fastq )
+ls $R2
+
+SAMP=$( basename $R1 | rev | cut -c 10- | rev )
+
+  bbduk.sh -Xmx32g threads=$THR in1=$R1 in2=$R2 \
+        out1=$TMPDIR/${SAMP}_T1.fastq out2=$TMPDIR/${SAMP}_T2.fastq \
+        ref=$ADAPTERS ktrim=r ordered \
+        k=23 mink=11 hdist=1 tbo tpe \
+        rcomp=f \
+        ow=t \
+        minlen=30 \
+        trimq=5 \
+        qtrim=rl
+rm $TMPDIR/*_R*.fastq
+[bgudenas@hpc02 PDX]$ cat ~/src/StarPDX.sh 
+#!/bin/bash
+
 echo "starPDX"
 THR=$1
 
@@ -13,9 +43,9 @@ mkdir -p ./Data/Human
 mkdir -p ./Data/Mouse
 
 
-R1=$(ls $TMPDIR/*T1.fastq )
+R1=$(ls $TMPDIR/*1.fastq )
 ls $R1
-R2=$(ls $TMPDIR/*T2.fastq )
+R2=$(ls $TMPDIR/*2.fastq )
 ls $R2
 
 SAMP=$( basename $R1 | rev | cut -c 10- | rev )
@@ -120,4 +150,3 @@ ls -lh $R2
     --alignSJDBoverhangMin 1
 
 fi
-
